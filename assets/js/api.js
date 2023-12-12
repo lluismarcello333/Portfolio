11160d32f86e37de6d05580cd30ab31dccf087a5
lluismarcello333 <lluismarcello333@gmail.com>
2023-12-12 10:03:23 -0300

    alter

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,5 @@
-function fetchProfileData() {
-    const url = 
-    fetch(url)
+async function fetchProfileData() {
+    const url = ('https://raw.githubusercontent.com/lluismarcello333/Portfolio/main/data/profile.json')
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
