9f47d814670185b7cc451af8c5c1d5db2ec5d27b
lluismarcello333 <lluismarcello333@gmail.com>
2023-12-12 11:06:15 -0300

    atualizando softSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,13 @@ function updateProfileInfo(profileData) {
     email.href = `mailto:${profileData.email}`
 }
 
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.profileSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
 })()
